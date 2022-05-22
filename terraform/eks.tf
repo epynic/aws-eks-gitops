91c461c0b2f8ac6b5afc120eadb295344be2e89f
Prasanha Kumar <epynic@gmail.com>
2022-05-22 15:56:08 +0000

    kustomize

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,23 +1,23 @@
 
-# module "eks_cluster_and_worker_nodes" {
-#   source                 = "./eks"
-#   eks_cluster_name       = "${var.infra_name}-eks-cluster"
+module "eks_cluster_and_worker_nodes" {
+  source                 = "./eks"
+  eks_cluster_name       = "${var.infra_name}-eks-cluster"
 
-#   eks_cluster_subnet_ids = flatten([module.vpc_for_eks.public_subnet_ids, module.vpc_for_eks.private_subnet_ids])
-#   public_subnet_ids = module.vpc_for_eks.public_subnet_ids
+  eks_cluster_subnet_ids = flatten([module.vpc_for_eks.public_subnet_ids, module.vpc_for_eks.private_subnet_ids])
+  public_subnet_ids = module.vpc_for_eks.public_subnet_ids
 
-#   disk_size         = "10"
-#   instance_types    = ["t3.small"]
-#   pblc_desired_size = 2
-#   pblc_max_size     = 3
-#   pblc_min_size     = 1
+  disk_size         = "10"
+  instance_types    = ["t3.small"]
+  pblc_desired_size = 2
+  pblc_max_size     = 3
+  pblc_min_size     = 1
 
-#   vpc_id = module.vpc_for_eks.vpc_id
-# }
+  vpc_id = module.vpc_for_eks.vpc_id
+}
 
-# output "eks_cluster_and_worker_nodes-cluster_name" {
-#   value = module.eks_cluster_and_worker_nodes.cluster_name
-# }
+output "eks_cluster_and_worker_nodes-cluster_name" {
+  value = module.eks_cluster_and_worker_nodes.cluster_name
+}
 
 
 # module "eks_cluster_argocd" {
